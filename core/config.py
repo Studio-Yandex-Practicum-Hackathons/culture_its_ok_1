d5aee4433a5e7d933c38b6a2810958c774b70bf0
SergeychUK92 <sergeytkachuk7988@yandex.ru>
2023-08-16 14:10:48 +0400

    changed google report_2

--- core/config.py
+++ core/config.py
@@ -2,7 +2,6 @@ import os
 
 import sentry_sdk
 from dotenv import load_dotenv
-from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
 
@@ -10,6 +9,4 @@ load_dotenv()
 def init_sentry():
     sentry_sdk.init(
         dsn=os.getenv("SENTRY_DSN"),
-        integrations=[DjangoIntegration()],
-        send_default_pii=True,
     )
