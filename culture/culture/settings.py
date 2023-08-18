13903a938fac6074bd33b97f11d815de4d616a3d
94R1K <dfdfdf.dfdf.1985@mail.ru>
2023-08-18 03:29:30 +0300

    Правки

--- culture/culture/settings.py
+++ culture/culture/settings.py
@@ -65,12 +65,12 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'culture_db',
-            'USER': 'db_user',
-            'PASSWORD': '1234567',
-            'HOST': 'postgres',
-            'PORT': '5432'
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT')
         }
     }
 
