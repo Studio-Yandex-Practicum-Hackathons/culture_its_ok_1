16d8d8a553bc6811f19792c9ecded02638f43523
danlaryushin <danlaryushin@gmail.com>
2023-08-18 17:52:54 +0300

    new routers in main.py, add some utils

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,25 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+
 from core.config import settings
 from core.logger import log_dec, logger_factory
-from handlers import new_user_router, start_router
+from handlers import (excursion_router, new_user_router, reflection_router,
+                      search_router, selection_router, start_router)
 
 
 @log_dec(logger=logger_factory(__name__))
 async def main():
-    bot = Bot(token=settings.bot.telegram_token)
+    bot = Bot(token=settings.bot.telegram_token, parse_mode='html')
 
     dispatcher = Dispatcher()
     dispatcher.include_routers(
         start_router,
-        new_user_router
+        new_user_router,
+        selection_router,
+        search_router,
+        excursion_router,
+        reflection_router,
     )
 
     await bot.delete_webhook(drop_pending_updates=True)
--- bot/utils.py
+++ bot/utils.py
@@ -2,6 +2,10 @@ from asyncio import sleep
 
 from aiogram import types
 
+from test_data import Expo, Route
+
+expo = Expo
+route = Route
 
 async def send_message_and_sleep(
     message: types.Message,
@@ -11,3 +15,39 @@ async def send_message_and_sleep(
 ):
     await message.answer(text, **kwargs)
     await sleep(delay)
+
+
+async def expo_data(expo_info):
+    expo.id = expo_info['id']
+    expo.name = expo_info['name']
+    expo.author = expo_info['author']
+    expo.address = expo_info['address']
+    expo.url = expo_info['url']
+    expo.how_to_get = expo_info['how_to_get']
+    expo.photo = expo_info['photo']
+    try:
+        expo.prologue = expo_info['prologue']
+    except:
+        expo.prologue = None
+    try:
+        expo.description = expo_info['description']
+    except:
+        expo.description = None
+    try:
+        expo.reflection = expo_info['reflection']
+    except:
+        expo.reflection = None
+    try:
+        expo.answer = expo_info['answer']
+    except:
+        expo.answer = None
+
+
+async def route_data(route_info):
+    route.length = len(route_info['objects'])
+    route.id = route_info['id']
+    route.address = route_info['address']
+    route.welcome = route_info['welcome']
+    route.objects = route_info['objects']
+    route.url = route_info['url']
+    route.description = route_info['description']
