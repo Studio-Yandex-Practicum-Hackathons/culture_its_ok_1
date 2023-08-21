a45370d4fbe0f4b4fd50c6dab622167717fcccd3
SergeychUK92 <sergeytkachuk7988@yandex.ru>
2023-08-21 21:57:43 +0400

    added google report_2

--- services/google_report.py
+++ services/google_report.py
@@ -1,5 +1,7 @@
 from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
+from string import ascii_uppercase
+
 
 SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -11,39 +13,37 @@ class GoogleReport:
     def __init__(self, credentials) -> None:
         self.credentials = credentials
         self.title = ""
-        self.headers = []
+        self.header = []
         self.email = ""
-        self.rows = 0
-        self.columns = 0
+        self.rows = []
 
-    def auth(self):
+    async def auth(self):
         credentials = Credentials.from_service_account_info(
             info=self.credentials, scopes=SCOPES)
-        service = discovery.build("sheets", "v4", credentials=credentials)
-        return service
+        return await discovery.build("sheets", "v4", credentials=credentials)
 
     def add_title(self, title):
         self.title = title
-        return self
 
-    def add_headers(self, *args):
+    def add_header(self, *args):
+        self.header = []
         for arg in args:
-            if len(arg) > self.columns:
-                self.columns = len(arg)
-            self.headers.append(arg)
-        self.add_rows(len(args))
-        return self
+            self.header.append(arg)
 
     def add_email(self, email):
         self.email = email
-        return self
 
-    def add_rows(self, num):
-        self.rows += num
-        return self
+    def add_rows(self, *args):
+        for arg in args:
+            self.rows.append(arg)
 
-    def create(self):
+    async def create(self):
         service = self.auth()
+        request_body = {
+            'majorDimension': 'ROWS',
+            'values': [self.header].extend(self.rows)
+            }
+        columnCount = len(max(self.header, key=lambda x: len(x)))
         spreadsheet_body = {
             "properties": {"title": self.title, "locale": "ru_RU"},
             "sheets": [
@@ -52,13 +52,24 @@ class GoogleReport:
                         "sheetType": "GRID",
                         "sheetId": 0,
                         "gridProperties": {
-                            "rowCount": self.rows,
-                            "columnCount": self.columns,
+                            "rowCount": len(self.rows),
+                            "columnCount": columnCount,
                         },
                     }
                 }
             ],
-            "body": self.headers,
         }
-
-        service.spreadsheets().create(body=spreadsheet_body)
+        request = await service.spreadsheets().create(body=spreadsheet_body)
+        response = await request.execute()
+        spreadsheet_id = response['spreadsheetId']
+        request = await service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id,
+            range=f'Лист1!A1:{ascii_uppercase[columnCount-1]}{len(self.rows)}',
+            valueInputOption='USER_ENTERED',
+            body=request_body
+        )
+        await request.execute()
+        return (
+            'https://docs.google.com/spreadsheets/d/' +
+            spreadsheet_id
+            )
