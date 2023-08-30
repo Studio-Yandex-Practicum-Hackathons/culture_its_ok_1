4b94d49eca5bb180860dbcab56d1a7eaefe2bf6f
Ubuntu 20.04.4 LTS <dcomrad@gmail.com>
2023-08-30 14:31:45 +0200

    add three types of report

--- bot/db/crud/progress.py
+++ bot/db/crud/progress.py
@@ -1,9 +1,38 @@
+from datetime import datetime
+
 from db.crud.base import CRUDBase
 from models import Progress
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class CRUDProgress(CRUDBase):
-    pass
+    async def get_user_usage(
+            self,
+            user_id: int,
+            session: AsyncSession):
+        db_obj = await session.execute(
+            select(self.model).where(
+                self.model.user_id == user_id
+            )
+        )
+        return len(db_obj.scalars().all())
+
+    async def get_all_by_route_and_range(
+            self,
+            route_id: int,
+            start: datetime,
+            end: datetime,
+            session: AsyncSession
+    ):
+        db_obj = await session.execute(
+            select(self.model).where(
+                self.model.route_id == route_id,
+                self.model.started_at >= start,
+                self.model.started_at <= end,
+            )
+        )
+        return db_obj.scalars().all()
 
 
 progress_crud = CRUDProgress(Progress)
